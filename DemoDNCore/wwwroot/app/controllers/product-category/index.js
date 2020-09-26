b0beac7c700470eaa53e521fb4e0cf4feabece95
DucBH <ducbh@dxmb.vn>
2020-09-27 02:49:04 +0700

    update treeview on database
    update treeview on database

--- DemoDNCore/wwwroot/app/controllers/product-category/index.js
+++ DemoDNCore/wwwroot/app/controllers/product-category/index.js
@@ -4,32 +4,75 @@
     }
     function loadData() {
         $.ajax({
-            url: "/Admin/ProductCategory/GetAll",
-            dataType: "json",
-            success: function (res) {
+            url: '/Admin/ProductCategory/GetAll',
+            dataType: 'json',
+            success: function (response) {
                 var data = [];
-                $.each(res, function (i, item) {
+                $.each(response, function (i, item) {
                     data.push({
                         id: item.Id,
                         text: item.Name,
                         parentId: item.ParentId,
-                        sortOrder: item.SortOrder,
+                        sortOrder: item.SortOrder
                     });
 
                 });
                 var treeArr = sd.unflattern(data);
-
+                treeArr.sort(function (a, b) {
+                    return a.sortOrder - b.sortOrder;
+                });
                 //var $tree = $('#treeProductCategory');
 
                 $('#treeProductCategory').tree({
                     data: treeArr,
-                    dnd: true
-                })
-            },
-            error: function (status) {
-                console.log(status);
-                sd.notify('Cannot loading data', 'error');
+                    dnd: true,
+                    onDrop: function (target, source, point) {
+                        console.log(target);
+                        console.log(source);
+                        console.log(point);
+                        var targetNode = $(this).tree('getNode', target);
+                        if (point === 'append') {
+                            var children = [];
+                            $.each(targetNode.children, function (i, item) {
+                                children.push({
+                                    key: item.id,
+                                    value: i
+                                });
+                            });
+
+                            //Update to database
+                            $.ajax({
+                                url: '/Admin/ProductCategory/UpdateParentId',
+                                type: 'post',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id,
+                                    items: children
+                                },
+                                success: function (res) {
+                                    loadData();
+                                }
+                            });
+                        }
+                        else if (point === 'top' || point === 'bottom') {
+                            $.ajax({
+                                url: '/Admin/ProductCategory/ReOrder',
+                                type: 'post',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id
+                                },
+                                success: function (res) {
+                                    loadData();
+                                }
+                            });
+                        }
+                    }
+                });
+
             }
-        })
+        });
     }
 }
