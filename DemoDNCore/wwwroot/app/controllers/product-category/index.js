aab4f95b7c8f9370bfe00ae751965410903b898c
DucBH <ducbh@dxmb.vn>
2020-09-27 02:00:44 +0700

    tree View
    add tree view

---
+++ DemoDNCore/wwwroot/app/controllers/product-category/index.js
@@ -0,0 +1,35 @@
+﻿var productCategoryController = function () {
+    this.initialize = function () {
+        loadData();
+    }
+    function loadData() {
+        $.ajax({
+            url: "/Admin/ProductCategory/GetAll",
+            dataType: "json",
+            success: function (res) {
+                var data = [];
+                $.each(res, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder,
+                    });
+
+                });
+                var treeArr = sd.unflattern(data);
+
+                //var $tree = $('#treeProductCategory');
+
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd: true
+                })
+            },
+            error: function (status) {
+                console.log(status);
+                sd.notify('Cannot loading data', 'error');
+            }
+        })
+    }
+}
